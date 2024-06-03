7d627a00b7761941fd7108c768720d239b927e25
TahaThinks <tahaa4587@gmail.com>
2024-06-03 21:33:39 +0400

    Scrap songs

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
 
-time = input("Which year do you want to travel to? Type the date in this format YYY-MM-DD: ")
-print(time)
+base_url = "https://www.billboard.com/charts/hot-100/"
+time = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+endpoint_url = f"{base_url}{time}/"
+print(endpoint_url)
+
+response = requests.get(endpoint_url)
+
+
+soup = BeautifulSoup(response.text, 'html.parser')
+print(f"{soup.title.text} of {time}")
+songs_tags = soup.find_all("h3", class_="u-line-height-125")
+songs = []
+for song_name in songs_tags:
+    songs.append(song_name.getText().strip())
+
+print(songs)
